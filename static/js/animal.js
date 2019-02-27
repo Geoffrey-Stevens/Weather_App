15824ac319ed4babb8bc1105ab65a89dc7478ed2
Geoffrey_Stevens <geoffreycmstevens@gmail.com>
2019-02-27 16:05:03 -0500

    nav update)

--- static/js/animal.js
+++ static/js/animal.js
@@ -1,3 +1,12 @@
+// get navbar from header
+$.get('../../components/header.html', function(res) {
+
+  $('header').html(res);
+});
+
+
+
+
 function animalPractice(){
   let url = 'https://learnwebcode.github.io/json-example/animals-1.json'
 }
